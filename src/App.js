821e059849a8183c730465385339df5154793e64
Muhsen Al-Bashaireh <muhsen.bashaireh@outlook.com>
2022-09-02 17:16:27 +0300

    Fetch monsters data on component mounting

--- src/App.js
+++ src/App.js
@@ -2,6 +2,21 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      monsters: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(monsters => {
+        this.setState({ monsters }, () => console.log('monsters updated'));
+      });
+  }
+
   render() {
     return <div>Monsters Rolodex</div>;
   }
